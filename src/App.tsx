cd15ca91079800803f8f06835cefec5006d7206d
Diego Samuel Arias Diaz <devdiego343@outlook.es>
2025-03-24 22:47:02 -0600

    attempts counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 
+import { useState } from "react";
 import { letters } from "../helpers/letters";
 import "./App.css";
 import { HangImage } from "./components/HangImage";
 
 function App() {
+
+  const [word] = useState ("COMPUTER");
+  const [hiddenWord] = useState ("_ ".repeat(word.length));
+
+  const [attempts, setAttempts] = useState(0)
+
+  const checkLetter = (letter: string) => {
+    console.log(letter);
+    setAttempts( Math.min(attempts + 1, 9) );
+  }
+
   return (
     <div className="App">
 
       {/*Images*/}
-      <HangImage imageNumber={ 9 }></HangImage>
+      <HangImage imageNumber={ attempts }></HangImage>
 
       {/*Hidden Word*/}
-      <p>_ _ _ _ _ _ _ _</p>
+      <p>{hiddenWord}</p>
 
       {/*Attempt counter*/}
-      <h3>Attempts: 0</h3>
+      <h3>Attempts: {attempts}</h3>
 
       {/*Letters Bottoms*/}
       {
         letters.map(letter => (
-          <button key={letter}>
+          <button 
+          onClick={() => checkLetter(letter)}
+          key={letter}>
             {letter}</button>
         ))
       }
