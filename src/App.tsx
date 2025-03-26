a9673d932b3e347219658e4ec50bdcade95be89f
Diego Samuel Arias Diaz <devdiego343@outlook.es>
2025-03-26 16:59:48 -0600

    Game logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { letters } from "../helpers/letters";
 import "./App.css";
 import { HangImage } from "./components/HangImage";
@@ -7,14 +7,45 @@ import { HangImage } from "./components/HangImage";
 function App() {
 
   const [word] = useState ("COMPUTER");
-  const [hiddenWord] = useState ("_ ".repeat(word.length));
-
+  const [hiddenWord, setHiddenWord] = useState ("_ ".repeat(word.length));
   const [attempts, setAttempts] = useState(0)
+  const [lose, setLose] = useState(false);
+  const [won, setWon] = useState(false);
+
+  //Determinate if the person lose
+  useEffect(() => {
+    if(attempts >= 9){
+      setLose(true);
+    }
+  }, [attempts]) // hooks
+
+  //Determinate if the person win
+
+useEffect(() => {
+    const currentHiddenWord = hiddenWord.split(" ").join("");
+    if (currentHiddenWord === word){
+      setWon(true);
+    }
+}, [hiddenWord])
 
   const checkLetter = (letter: string) => {
-    console.log(letter);
-    setAttempts( Math.min(attempts + 1, 9) );
-  }
+
+    if(lose) return; //Validation if lost cannot enter more letters
+
+    if(!word.includes(letter)) {
+      setAttempts( Math.min(attempts + 1, 9) );
+      return;
+    } //logic validate attempts < 9
+
+    const hiddenWordArray = hiddenWord.split(" ");
+
+    for(let i= 0; i < word.length; i++){
+      if (word[i] === letter){
+        hiddenWordArray [i] = letter;
+      }
+    }
+    setHiddenWord(hiddenWordArray.join(" "));
+  } //Function show hidden word
 
   return (
     <div className="App">
@@ -28,6 +59,20 @@ function App() {
       {/*Attempt counter*/}
       <h3>Attempts: {attempts}</h3>
 
+      {/*Message if lose*/}
+      {
+        (lose)
+        ? <h2>You lose the word was: { word } :( </h2>
+        :""
+      }
+
+      {/*Message if won*/}
+      {
+        (won)
+        ? <h2>Congratulations you won :)</h2>
+        :""
+      }
+
       {/*Letters Bottoms*/}
       {
         letters.map(letter => (
@@ -36,7 +81,7 @@ function App() {
           key={letter}>
             {letter}</button>
         ))
-      }
+      } {/*Array alphabet*/}
       
 
     </div>
